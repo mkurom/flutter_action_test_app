d08cfc03ff79c60ec77e10ffbe99dc384a761db9
yasudamakoto <hare.v1v1enne@gmail.com>
2021-11-14 18:12:40 +0900

    make android code

--- android/app/src/main/kotlin/com/sample/flutter_action_test_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/sample/flutter_action_test_app/MainActivity.kt
@@ -1,6 +1,41 @@
 package com.sample.flutter_action_test_app
 
+import androidx.annotation.NonNull
+// Flutter
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+// Android
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "com.sample.flutter_action_test_app/battery"
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+      // Note: this method is invoked on the main thread.
+      // TODO
+    }
+  }
+
+  private fun getBatteryLevel(): Int {
+    val batteryLevel: Int
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    } else {
+      val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+      batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+    }
+
+    return batteryLevel
+  }
 }
